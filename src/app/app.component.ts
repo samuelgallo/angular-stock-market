d8138c8ff7307aa31547d30e20c7983496f8faf3
Samuel Gallo <samuel@samuelgallo.com>
2023-03-21 11:59:36 -0400

    css encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  // encapsulation: ViewEncapsulation.None,
+  // // use global css without encapsulation
+  // encapsulation: ViewEncapsulation.Emulated,
+  // // angular creates shimmed css
+  // encapsulation: ViewEncapsulation.Native,
+  // // angular uses shadow roots
 })
 export class AppComponent {
   title = 'stock-market';
