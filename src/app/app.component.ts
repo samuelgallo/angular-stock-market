f871b9736ae622629d653761dcb6319865ea273b
Samuel Gallo <samuel@samuelgallo.com>
2023-04-10 10:57:46 -0400

    stock item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+
+  preserveWhitespaces: false,
 
   // encapsulation: ViewEncapsulation.None,
   // // use global css without encapsulation
@@ -12,6 +15,19 @@ import { Component, ViewEncapsulation } from '@angular/core';
   // encapsulation: ViewEncapsulation.Native,
   // // angular uses shadow roots
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-market';
+
+  //public stockObj!: Stock;
+  public stocks!: Stock;
+  public $vent!: Stock;
+
+  ngOnInit(): void {
+    this.stocks = new Stock('Test Stock Company', 'TSC', 85, 80);
+  }
+
+  onToggleFavorite(stocks: Stock) {
+    console.log('Favorite for stock ', stocks, ' was triggered');
+    this.stocks.favorite = !this.stocks.favorite;
+  }
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Stock } from '../../model/stock';
 
@@ -33,68 +33,80 @@ import { Stock } from '../../model/stock';
 //   </div>`,
 //   styleUrls: ['./stock-item.component.css'],
 // })
-export class StockItemComponent implements OnInit {
-  // public name!: string;
-  // public code!: string;
-  // public price!: number;
-  // public previousPrice!: number;
-  // public positiveChange!: boolean;
-  // public favorite!: boolean;
+// export class StockItemComponent implements OnInit {
+//   // public name!: string;
+//   // public code!: string;
+//   // public price!: number;
+//   // public previousPrice!: number;
+//   // public positiveChange!: boolean;
+//   // public favorite!: boolean;
 
-  public stock!: Stock;
-  public stockClasses: any;
-  public stockStyles: any;
+//   public stock!: Stock;
+//   public stockClasses: any;
+//   public stockStyles: any;
 
-  public stocks!: Array<Stock>;
+//   public stocks!: Array<Stock>;
 
-  constructor() {}
+//   constructor() {}
 
-  ngOnInit() {
-    // this.name = 'Test Stock Company';
-    // this.code = 'TSC';
-    // this.price = 85;
-    // this.previousPrice = 80;
-    // this.positiveChange = this.price >= this.previousPrice;
-    // this.favorite = false;
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 90);
+//   ngOnInit() {
+//     // this.name = 'Test Stock Company';
+//     // this.code = 'TSC';
+//     // this.price = 85;
+//     // this.previousPrice = 80;
+//     // this.positiveChange = this.price >= this.previousPrice;
+//     // this.favorite = false;
+//     this.stock = new Stock('Test Stock Company', 'TSC', 85, 90);
 
-    this.stocks = [
-      new Stock('Test Stock Company', 'TSC', 85, 80),
-      new Stock('Second Stock Company', 'SSC', 10, 20),
-      new Stock('Last Stock Company', 'LSC', 876, 765),
-    ];
+//     this.stocks = [
+//       new Stock('Test Stock Company', 'TSC', 85, 80),
+//       new Stock('Second Stock Company', 'SSC', 10, 20),
+//       new Stock('Last Stock Company', 'LSC', 876, 765),
+//     ];
 
-    let diff = this.stock.price / this.stock.previousPrice - 1;
-    let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      positive: this.stock.isPositiveChange(),
-      negative: !this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange,
-    };
-    this.stockStyles = {
-      color: this.stock.isPositiveChange() ? 'green' : 'red',
-      'font-size': largeChange ? '1.2em' : '0.8em',
-    };
+//     let diff = this.stock.price / this.stock.previousPrice - 1;
+//     let largeChange = Math.abs(diff) > 0.01;
+//     this.stockClasses = {
+//       positive: this.stock.isPositiveChange(),
+//       negative: !this.stock.isPositiveChange(),
+//       'large-change': largeChange,
+//       'small-change': !largeChange,
+//     };
+//     this.stockStyles = {
+//       color: this.stock.isPositiveChange() ? 'green' : 'red',
+//       'font-size': largeChange ? '1.2em' : '0.8em',
+//     };
+//   }
+
+//   // toggleFavorite(event: Event) {
+//   //   console.log(
+//   //     'We are toggling the favorite state for this stock',
+//   //     event.preventDefault
+//   //   );
+//   //   this.stock.favorite = !this.stock.favorite;
+//   // }
+//   toggleFavorite(event: Event, index: any) {
+//     console.log(
+//       'We are toggling the favorite state for this stock',
+//       index,
+//       event
+//     );
+//     this.stocks[index].favorite = !this.stocks[index].favorite;
+//   }
+
+//   trackStockByCode(index: any, stock: Stock) {
+//     return stock.code;
+//   }
+// }
+export class StockItemComponent {
+  @Input() public stocks!: Stock;
+  @Output() private toggleFavorite!: EventEmitter<Stock>;
+
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
   }
 
-  // toggleFavorite(event: Event) {
-  //   console.log(
-  //     'We are toggling the favorite state for this stock',
-  //     event.preventDefault
-  //   );
-  //   this.stock.favorite = !this.stock.favorite;
-  // }
-  toggleFavorite(event: Event, index: any) {
-    console.log(
-      'We are toggling the favorite state for this stock',
-      index,
-      event
-    );
-    this.stocks[index].favorite = !this.stocks[index].favorite;
-  }
-
-  trackStockByCode(index: any, stock: Stock) {
-    return stock.code;
+  onToggleFavorite(event: Event) {
+    this.toggleFavorite.emit(this.stocks);
   }
 }
